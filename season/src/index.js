ff0faa926b110ae5eb5e09395bda089f7041ece5
Tauseef <tauseef76@outlook.com>
2020-03-20 16:25:30 +0530

    pics installed axios

--- season/src/index.js
+++ season/src/index.js
@@ -1,33 +1,47 @@
 import React from "react";
 import ReactDom from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component{
+
+  //alternate way to declare state... no need to declare constructor
+  state = {lat: null, errorMessage:""};
+
+  /*
+  //==============constructor()=================//
   constructor(props){
     super(props);
-
     this.state = {lat:null, errorMessage: ""};
-
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState ({lat: position.coords.latitude})
-      },
-      (err) => {
-        this.setState({errorMessage: err.message})
-      }
-    );
-
   }
-  //=============render(): Only render/display work================//
-  render(){
+*/
+//=================componentDidMount()=======================//
+  componentDidMount(){
+      window.navigator.geolocation.getCurrentPosition(
+      (position) => this.setState ({lat: position.coords.latitude}),
+      (err) => this.setState({errorMessage: err.message})
+    );
+  }
+
+//==========================helper()===========================//
+renderContent(){
     if(this.state.errorMessage && !this.state.lat){
       return <div>Error:{this.state.errorMessage}</div>
     }
     else if (!this.state.lat && !this.state.errorMessage) {
-      return (<div>Loading Data.....</div>);
+      return <Spinner />
     }
     else{
-      return(<div>Latitude: {this.state.lat}</div>);
+      return(<div><SeasonDisplay lat={this.state.lat} /></div>);
     }
+  }
+  //=============render(): Only render/display work================//
+  render(){
+    return(
+      <div className = "border-red">
+        {this.renderContent()}
+      </div>
+    );
   };
 }
 
